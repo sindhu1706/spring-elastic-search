d7e5eb5c2eb2f0eba2f93ab7a16262dbecb323bc
Sindhuri Kalyanapu <sindhurik620@gmail.com>
2019-11-23 18:28:00 +0530

    first commit

---
+++ src/main/java/com/elastic/search/elasticSearchUtil/ElasticSearchUtilApplication.java
@@ -0,0 +1,13 @@
+package com.elastic.search.elasticSearchUtil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticSearchUtilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticSearchUtilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elastic/search/elasticSearchUtil/ElasticSearchUtilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elastic.search.elasticSearchUtil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticSearchUtilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
